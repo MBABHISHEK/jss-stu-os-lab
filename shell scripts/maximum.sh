9f804e8008c316ac6dd46cd67392f3dd7595126f
ABHI <mba384420@gmail.com>
2023-07-11 06:22:21 +0530

    new

---
+++ shell scripts/maximum.sh
@@ -0,0 +1,28 @@
+#1/bin/bash
+
+echo "ENTER THE NUMBER OF ELEMENTS OF THE ARRAY"
+read n
+
+declare -A arr;
+echo "ENTER THE ELEMENTS TO THE ARRAY"
+
+for ((i=0; i<n; i++))
+do
+   read arr["$i"];
+done
+
+
+max=${arr[0]}
+
+for ((i=1; i<n; i++))
+do
+   if [[ "$arr[$i]" -gt "$max" ]]; 
+   then
+        max=${arr[$i]}
+    if
+ done
+
+ echo "THE LAEGEST VALUE IN THE ARRAY>> $MAX"   
+
+
+
