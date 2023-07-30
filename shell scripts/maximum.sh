f8b2fc3f50afdf4439c1e190eee4a23e65d6a81d
ABHISHEK M B <117739148+MBABHISHEK@users.noreply.github.com>
2023-07-30 11:18:12 +0530

    Update maximum.sh

--- shell scripts/maximum.sh
+++ shell scripts/maximum.sh
@@ -1,28 +1,24 @@
-#1/bin/bash
+#!/bin/bash
 
 echo "ENTER THE NUMBER OF ELEMENTS OF THE ARRAY"
 read n
 
-declare -A arr;
+declare -a arr;  # Corrected from declare -A to declare -a for indexed array
 echo "ENTER THE ELEMENTS TO THE ARRAY"
 
 for ((i=0; i<n; i++))
 do
-   read arr["$i"];
+    read arr["$i"]
 done
-
 
 max=${arr[0]}
 
 for ((i=1; i<n; i++))
 do
-   if (( "$arr[$i]" -gt "$max" )); 
-   then
-        max=${arr[$i]}
+    if (( ${arr[i]} > max )); # Corrected from "$arr[$i]" to ${arr[i]}
+    then
+        max=${arr[i]}
     fi
- done
+done
 
- echo "THE LAEGEST VALUE IN THE ARRAY>> $max"   
-
-
-
+echo "THE LARGEST VALUE IN THE ARRAY: $max"
